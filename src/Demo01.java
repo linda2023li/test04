a61664adc91cfa9da25666b836516a8a29409e78
linda <linda@163.com>
2023-08-28 12:33:15 +0800

    000000

---
+++ src/Demo01.java
@@ -0,0 +1,21 @@
+/**
+ * @ClassName Demo01
+ * @Description TODO
+ * @Author li.liu
+ * @Date 2023/8/28 10:55
+ * @Version 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+        System.out.println("001");
+    }
+
+}
---
+++ src/Demo02.java
@@ -0,0 +1,8 @@
+/**
+ *@ClassName Demo02
+ *@Description  TODO
+ *@Author li.liu
+ *@Date 2023/8/28 12:12
+ *@Version 1.0
+ */public class Demo02 {
+}
