6680a8d0a4ff0c94a17ef4c51054e654be6e3dd4
linda <linda@163.com>
2023-08-28 12:12:39 +0800

    第二次提交

---
+++ src/Demo02.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Demo02
+ * @Description TODO
+ * @Author li.liu
+ * @Date 2023/8/28 12:12
+ * @Version 1.0
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println("Demo02");
+    }
+}
